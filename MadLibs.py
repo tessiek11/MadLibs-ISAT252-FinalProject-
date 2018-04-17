2254817793fcb4382fca7cd49b8bd67706dbc88b
tessiek11 <36481177+tessiek11@users.noreply.github.com>
2018-04-17 14:10:50 -0400

    Add files via upload

---
+++ MadLibs.py
@@ -0,0 +1,28 @@
+
+
+
+adjective1 = raw_input("Tell me an adjective, and click enter. ")
+name1 = raw_input("Tell me a name, and click enter. ")
+superpower1 = raw_input("Tell me a superpower, and click enter. ")
+name2 = raw_input("Tell me the same name, and click enter. ")
+verb1 = raw_input("Tell me a verb, and click enter. ")
+verb2 = raw_input("Tell me a another verb but ending in -ed, and click enter. ")
+name3 = raw_input("Tell me the same name, again, and click enter. ")
+emotion1 = raw_input("Tell me an emotion, and click enter. ")
+adjective2 = raw_input("Tell me another adjective, and click enter. ") 
+job1 = raw_input("Tell me a job, and click eneter. ")
+
+
+
+print "There once was a " + adjective1
+print "duck named" + name1
+print "! It had always been a dream of the duck to be a superhero with " + superpower1
+print "as a superpower. One day, " + name2
+print " thought they would try to achieve their dreams, so they tried to " + verb1
+print " but accidentally " + verb2
+print " in front of the entire pond! " + name3
+print " was incredibly " + emotion1
+print ". The " + adjective2 
+print " duck decided to give up on their dreams of being a superhero and just become a " + job1
+print " instead. "
+print " The End. "
