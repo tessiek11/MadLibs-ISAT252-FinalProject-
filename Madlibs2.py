4d36286e42af839082e35583af5bab9848e5de84
tessiek11 <36481177+tessiek11@users.noreply.github.com>
2018-04-18 11:26:18 -0400

    Add files via upload

---
+++ Madlibs2.py
@@ -0,0 +1,23 @@
+def madlib ():
+	print ("Welcome to our Mad Lib: The Story of a Duck")
+    
+adjective1 = input("Tell me an adjective, and click enter. ")
+name1 = input(" Tell me a name, and click enter. ")
+superpower1 = input(" Tell me a superpower, and click enter. ")
+name2 = input(" Tell me the same name, and click enter. ")
+verb1 = input(" Tell me a verb, and click enter. ")
+verb2 = input(" Tell me a another verb but ending in -ed, and click enter. ")
+name3 = input(" Tell me the same name, again, and click enter. ")
+emotion1 = input(" Tell me an emotion, and click enter. ")
+adjective2 = input(" Tell me another adjective, and click enter. ") 
+job1 = input(" Tell me a job, and click eneter. ")
+
+
+
+print (" There once was a " + adjective1,  "duck named " + name1 + "!" ) 
+print (" It had always been a dream of the duck to be a superhero with " + superpower1 + " as a superpower. ")
+print (" One day, " + name2 + " thought they would try to achieve their dreams, so they tried to " + verb1 + " but accidentally" + verb2 + "in front of the entire pond! ") 
+print (" thought they would try to achieve their dreams, so they tried to " + verb1 )
+print ( name3 + " was incredibly " + emotion1 )
+print (" The " + adjective2 + " duck decided to give up on their dreams of being a superhero and just become a " + job1 + " instead. ")
+print (" The End. ")
